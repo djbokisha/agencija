f1baaada565f8d0980063f47d97bb8cb7abdf776
djbokisha <63730837+djbokisha@users.noreply.github.com>
2020-12-02 22:39:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$(window).on("scroll",function(){
+    var scroll = $(window).scrollTop();
+    if(scroll >= 50){
+        $(".sticky").addClass("stickyAdd");  
+    } else{
+        $(".sticky").removeClass("stickyAdd");  
+
+    }
+});
+
+
+
+///////
+const naslov = "Full digitalna usluga";
+let i = 0;
+
+const kuckanje = () => {
+    if(i<naslov.length){
+        document.querySelector(".tipkajuciNaslov").innerHTML += naslov.charAt(i);
+        i++;
+
+        setTimeout(kuckanje,150);
+    }
+}
+kuckanje()
